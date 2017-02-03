3ad268fd8fe6027f5020e5a51e5149647d68b677
Onyedikachi Kalu <dikachik@yahoo.com>
2017-02-03 12:45:50 -0500

    fix lab2

---
+++ l2/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,9 @@
+package home.dikachi.Documents.csci2020u.lab02.src.main.java.csci2020u.lab02;
+
+public class SimpleTest{
+public static void main(String[] args)
+{
+	System.out.println("HelloWorld");
+
+}
+}
